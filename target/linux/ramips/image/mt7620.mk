1b9c4f680642d8c5377d39ac22c7d77f51768a81
Yogi Hermawan <yogist.xda@gmail.com>
2024-06-12 10:27:35 +0000

    ramips: update BoltBL201 to match stock bootloader
    Comment:
    No need to change the bootloader using breedweb anymore.
    But breedweb can still be used.
    
    Changes:
    1. Update device baud rate
    2. Update some of leds and button gpios
    3. Update partition layout to match the stock layout
    4. Update missing package depends for usb
    
    Signed-off-by: Yogi Hermawan <yogist.xda@gmail.com>

--- target/linux/ramips/image/mt7620.mk
+++ target/linux/ramips/image/mt7620.mk
@@ -164,7 +164,7 @@ define Device/bolt_bl201
   IMAGE_SIZE := 15872k
   DEVICE_VENDOR := Bolt
   DEVICE_MODEL := BL201
-  DEVICE_PACKAGES := kmod-mt76x2
+  DEVICE_PACKAGES := kmod-mt76x2 kmod-usb2 kmod-usb-ohci
 endef
 TARGET_DEVICES += bolt_bl201
 
